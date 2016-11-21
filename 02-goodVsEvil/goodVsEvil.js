1050cad5de37511961f343d25ad0b79286e0b1b0
Neal Smeathers <nealsmeathers@Neals-MacBook-Pro.local>
2016-11-21 18:43:51 +0000

    02-goodVsEvil finished

--- 02-goodVsEvil/goodVsEvil.js
+++ 02-goodVsEvil/goodVsEvil.js
@@ -1,5 +1,48 @@
 function goodVsEvil (good, evil) {
+  var sumGood = 0;
+  var sumEvil = 0;
+  var goodArr = good.split(' ');
+  var evilArr = evil.split(' ');
 
+  for (var i = 0; i < goodArr.length; i++) {
+    if (i === 0) {
+      sumGood += Number(goodArr[i]);
+    }
+    else if (i === 1) {
+      sumGood += (Number(goodArr[i])) * 2;
+    }
+    else if (i === 2 || i === 3) {
+      sumGood += (Number(goodArr[i])) * 3;
+    }
+    else if (i === 4) {
+      sumGood += (Number(goodArr[i])) * 4;
+    }
+    else if (i === 5) {
+      sumGood += (Number(goodArr[i])) * 10;
+    }
+  }
+
+  for (var j = 0; j < evilArr.length; j++) {
+    if (j === 0) {
+      sumEvil += Number(evilArr[j]);
+    }
+    else if (j === 1 || j === 2 || j === 3) {
+      sumEvil += (Number(evilArr[j])) * 2;
+    }
+    else if (j === 4) {
+      sumEvil += (Number(evilArr[j])) * 3;
+    }
+    else if (j === 5) {
+      sumEvil += (Number(evilArr[j])) * 5;
+    }
+    else if (j === 6) {
+      sumEvil += (Number(evilArr[j])) * 10;
+    }
+  }
+
+  if (sumGood > sumEvil) return 'Battle Result: Good triumphs over Evil';
+  if (sumGood < sumEvil) return 'Battle Result: Evil eradicates all trace of Good';
+  return 'Battle Result: No victor on this battle field'
 }
 
 module.exports = goodVsEvil;
--- spec/02-goodVsEvil.spec.js
+++ spec/02-goodVsEvil.spec.js
@@ -3,8 +3,30 @@
 import { expect } from 'chai';
 import goodVsEvil from '../02-goodVsEvil/goodVsEvil';
 
+var evilWin = 'Battle Result: Evil eradicates all trace of Good';
+var goodWin = 'Battle Result: Good triumphs over Evil';
+var draw = 'Battle Result: No victor on this battle field';
+
 describe('goodVsEvil', function () {
   it('is a function', function () {
     expect(goodVsEvil).to.be.a('function');
+  });
+  it('takes two arguments', function () {
+    expect(goodVsEvil.length).to.equal(2);
+  });
+  it('returns win for evil', function () {
+    expect(goodVsEvil('1 1 1 1 1 1', '1 1 1 1 1 1 1')).to.equal(evilWin);
+  });
+  it('returns win for good', function () {
+    expect(goodVsEvil('0 0 0 0 0 10', '0 0 0 0 0 0 9')).to.equal(goodWin);
+  });
+  it('returns win for evil', function () {
+    expect(goodVsEvil('1 1 1 0 0 1', '3 1 0 0 1 0 1')).to.equal(evilWin);
+  });
+  it('returns win for good', function () {
+    expect(goodVsEvil('2 2 2 2 2 2', '1 1 1 2 2 2 0')).to.equal(goodWin);
+  });
+  it('returns a draw', function () {
+    expect(goodVsEvil('3 2 2 3 2 1', '5 4 2 2 3 2 0')).to.equal(draw);
   });
 });
